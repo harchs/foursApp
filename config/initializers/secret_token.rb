211540f404ddd03a490dc8b6a90b1d44e89f402f
Harold Rangel Charris <harold.rangel@koombea.com>
2012-03-14 14:05:25 -0500

    project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foursquareapp::Application.config.secret_token = '5d04df165f0a96f7b25be324bd3205999790a1c235d2f922923c6c06342f15fbaf00362ae431ae632aa16b84f8694148b3a365f3a48a8412c4dc2a3c46120473'
